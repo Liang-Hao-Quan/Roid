49193a6492be557d00751b189ab1c1e7f7041b79
Haoquan Liang <haoquan@seas.upenn.edu>
2022-01-16 16:16:00 -0500

    Step 13 Done

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -4,17 +4,45 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // some public variables that can be used to tune the Ship’s movement
+    public Vector3 forceVector;
+    public float rotationSpeed;
+    public float rotation;
+
+    // Use this for initialization
     void Start()
     {
-        
+        // Vector3 default initializes all components to 0.0f
+        forceVector.x = 1.0f;
+        rotationSpeed = 2.0f;
+    }
+    /* forced changes to rigid body physics parameters should be done through the FixedUpdate() method, not the Update() method
+    */
+    void FixedUpdate()
+    {
+        // force thruster
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(forceVector);
+        }
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rotation += rotationSpeed;
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            GetComponent<Rigidbody>().MoveRotation(rot);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rotation -= rotationSpeed; Quaternion rot = Quaternion.Euler(new
+            Vector3(0, rotation, 0));
+            GetComponent<Rigidbody>().MoveRotation(rot);
+            //GetComponent<Rigidbody>().Rotate(0, -2.0f, 0.0f );
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 updatedPosition = gameObject.transform.position;
-        updatedPosition.x += .001f;
-        gameObject.transform.position = updatedPosition;
+        // nothing in here now that movement is done by physics
     }
 }
