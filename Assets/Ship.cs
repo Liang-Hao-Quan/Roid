58a081ef64e9d0db1165e466c88582ad60e83393
Haoquan Liang <haoquan@seas.upenn.edu>
2022-01-16 16:57:01 -0500

    Step 15 Done

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -30,19 +30,37 @@ public class Ship : MonoBehaviour
             rotation += rotationSpeed;
             Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
             GetComponent<Rigidbody>().MoveRotation(rot);
+            //gameObject.transform.Rotate(0, 2.0f, 0.0f );
         }
         else if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            rotation -= rotationSpeed; Quaternion rot = Quaternion.Euler(new
-            Vector3(0, rotation, 0));
+            rotation -= rotationSpeed;
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
             GetComponent<Rigidbody>().MoveRotation(rot);
-            //GetComponent<Rigidbody>().Rotate(0, -2.0f, 0.0f );
+            //gameObject.transform.Rotate(0, -2.0f, 0.0f );
         }
     }
 
-    // Update is called once per frame
+    public GameObject bullet; // the GameObject to spawn
     void Update()
     {
-        // nothing in here now that movement is done by physics
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire! " + rotation);
+            /* we don’t want to spawn a Bullet inside our ship, so some
+            Simple trigonometry is done here to spawn the bullet
+            at the tip of where the ship is pointed.
+            */
+            Vector3 spawnPos = gameObject.transform.position;
+            spawnPos.x += 1.5f * Mathf.Cos(rotation * Mathf.PI / 180);
+            spawnPos.z -= 1.5f * Mathf.Sin(rotation * Mathf.PI / 180);
+            // instantiate the Bullet
+            GameObject obj = Instantiate(bullet, spawnPos, Quaternion.identity) as GameObject;
+            // get the Bullet Script Component of the new Bullet instance
+            BulletScript b = obj.GetComponent<BulletScript>();
+            // set the direction the Bullet will travel in
+            Quaternion rot = Quaternion.Euler(new Vector3(0, rotation, 0));
+            b.heading = rot;
+        }
     }
 }
